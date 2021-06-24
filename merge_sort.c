214c9800e619e06a042ebddd672d288ba3e2f0fe
Elderny <84969656+elderny@users.noreply.github.com>
2021-06-24 08:57:44 -0700

    Add files via upload

---
+++ merge_sort.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printArray(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+void merge(int a[], int low, int mid, int high)
+{
+    int i, j, k, b[high + 1];
+    i = low;
+    j = mid + 1;
+    k = low;
+    while (i <= mid && j <= high)
+    {
+        if (a[i] < a[j])
+        {
+            b[k] = a[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            b[k] = a[j];
+            j++;
+            k++;
+        }
+    }
+    while (i <= mid)
+    {
+        b[k] = a[i];
+        i++;
+        k++;
+    }
+    while (j <= high)
+    {
+        b[k] = a[j];
+        j++;
+        k++;
+    }
+    for (int i = low; i <= high; i++)
+    {
+        a[i] = b[i];
+    }
+}
+
+void mergeSort(int a[], int low, int high)
+{
+    int mid;
+    if (low < high)
+    {
+        mid = (low + high) / 2;
+        mergeSort(a, low, mid);
+        mergeSort(a, mid + 1, high);
+        merge(a, low, mid, high);
+    }
+}
+
+int main()
+{
+    // int a[] = {5, 0, 1, 8, 6, 9};
+    int a[] = {5, 10, 1, 8, 6, 9, 2, 0, 3};
+    int n = sizeof(a) / sizeof(int);
+    printArray(a, n);
+    mergeSort(a, 0, n - 1);
+    printArray(a, n);
+    return 0;
+}
